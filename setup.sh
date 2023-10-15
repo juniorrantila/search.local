9bc5133b9db6375487207d4423f63abef2392603
Junior Rantila <junior.rantila@gmail.com>
2023-10-15 14:04:02 +0200

    Download ggml-tiny.en.bin on setup

--- setup.sh
+++ setup.sh
@@ -9,7 +9,11 @@ mkdir -p ./dataset/youtube/feeds
 mkdir -p ./dataset/youtube/audio
 mkdir -p ./dataset/youtube/transcript-srt
 mkdir -p ./dataset/index
+mkdir -p ./dataset/models
 
 mkdir -p ./vendor/whisper.cpp/build
 cmake -S ./vendor/whisper.cpp -B ./vendor/whisper.cpp/build
 ninja -C ./vendor/whisper.cpp/build
+
+./vendor/whisper.cpp/models/download-ggml-model.sh tiny.en
+mv ./vendor/whisper.cpp/models/ggml-tiny.en.bin ./dataset/models
